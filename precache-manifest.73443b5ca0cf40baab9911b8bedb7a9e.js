3442352f0eb6eac3eff5a632213dfc4b0c7d3048
AnsurM <ansurmehdi@gmail.com>
2020-01-28 15:25:53 +0500

    Updates

---
+++ precache-manifest.73443b5ca0cf40baab9911b8bedb7a9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93614ed0f8eaed2c660dd2d86f6f582",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "cbfff0ae3f0de969099d",
+    "url": "/portfolio-react/static/css/main.d86729c6.chunk.css"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/portfolio-react/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-react/static/js/2.f175dcd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbfff0ae3f0de969099d",
+    "url": "/portfolio-react/static/js/main.9cafffa8.chunk.js"
+  },
+  {
+    "revision": "4355c1b3504c43a9fad0",
+    "url": "/portfolio-react/static/js/runtime-main.53b78f2a.js"
+  }
+]);
